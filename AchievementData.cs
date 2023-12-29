d4c3bea2bb0a398b28da80c89e30138f9a97df30
Elyes <126920308+techieeDev@users.noreply.github.com>
2023-12-29 16:24:27 +0100

    Add files via upload

---
+++ AchievementData.cs
@@ -0,0 +1,137 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class AchievementData : MonoBehaviour
+{
+
+    [Range(11290, 21290)]
+    public int AchievementId;
+
+    public string _achievement_name;
+    public Sprite _achievement_sprite;
+
+    public AchievementType achievementType;
+    public bool isUnlocked;
+    public bool canFadeIn;
+    internal bool isSameId;
+    internal bool isTimerOn;
+
+    private float _color_t_ = 0.1f;
+    internal float timer = 7;
+    internal float clock = 0;
+
+    void Start()
+    {
+        GameEvents.current.onItemPickup += AchievementUnlock;
+
+        GameEvents.current.onEntranceAchievement += AchievementUnlock;
+
+        GameEvents.current.onItemPickup += GetId;
+
+        GameEvents.current.onEntranceAchievement += GetId;
+
+        AchievementCheck(AchievementManager.current.AchievementStore, AchievementManager.current.AchievemntIds);
+    }
+    void ShowItemIcon()
+    {
+        if (canFadeIn)
+        {
+            if (isSameId)
+            {
+                Image AchievementPopup = GameObject.Find("AchievementPopup").GetComponent<Image>();
+
+                AchievementPopup.sprite = _achievement_sprite;
+
+                
+
+                if (Time.time > clock && timer > 0)
+                {
+                    isTimerOn = true;
+                    Debug.Log(timer);
+                    clock = Time.time + 1;
+                    timer--;
+                }
+                else if (isTimerOn && timer > 0)
+                {
+                    AchievementPopup.color = Color.Lerp(AchievementPopup.color, Color.white, _color_t_);
+                }
+                else if(timer <= 0)
+                {
+                    AchievementPopup.color = Color.Lerp(AchievementPopup.color, new Color(0, 0, 0, 0), _color_t_ * 0.08f);
+                    isTimerOn = false;
+                }
+            }
+        }
+    }
+
+
+    void AchievementUnlock(AchievementData data)
+    {
+        if(data.AchievementId == AchievementId)
+        {
+            Debug.Log(AchievementId);
+            
+            if (isUnlocked == false)
+            {
+                AudioSource _sfxplayer = GameObject.Find("AchievementSFX").GetComponent<AudioSource>();
+
+                isUnlocked = true;
+                canFadeIn = true;
+
+                _sfxplayer.Play();
+                
+                AchievementManager.current.RegisterAchievement(isUnlocked, AchievementId);
+
+                AchievementManager.current.SaveLoadedAchievements("_achievements.save", "_ids.save");
+
+                GameEvents.current.onItemPickup -= AchievementUnlock;
+            }
+        }
+
+        
+    }
+
+
+    void AchievementCheck(bool[] x, int[] _IDs)
+    {
+        if(x != null && _IDs != null)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if (_IDs[i] == AchievementId)
+                {
+                    isUnlocked = x[i];
+                    break;
+                }
+            }
+        }
+
+    }
+
+    void GetId(AchievementData data)
+    {
+        if(data.AchievementId == AchievementId)
+        {
+            isSameId = true;
+        }
+        else
+        {
+            isSameId = false;
+        }
+        
+    }
+
+    public void Update()
+    {
+        ShowItemIcon();
+    }
+
+
+}
+
+
+
+public enum AchievementType
+{
+    InteractionUnlock,
+    EntranceUnlock,
+}
---
+++ AchievementManager.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class AchievementManager : MonoBehaviour
+{
+
+    
+
+    public int[] AchievemntIds = new int[10000];
+    public bool[] AchievementStore = new bool[10000];
+
+
+    public static AchievementManager current;
+    static Loader loader = new Loader();
+
+    void Awake()
+    {
+        current = this;
+    }
+
+    void Start()
+    {
+        loader.Load("_achievements.save", "_ids.save");
+    }
+
+    public void RegisterAchievement(bool isUnlocked, int id)
+    {
+        
+        //     ~ Set Achievement
+        int y = 0;
+        foreach(int item in current.AchievemntIds)
+        {      
+            if(item == 0)
+            {
+                current.AchievemntIds[y] = id;
+                current.AchievementStore[y] = true;
+                Debug.Log("ID's Registered");
+                break;
+            }
+            y++; 
+        }
+    }
+
+
+    //    ~ After Registering New Achievement
+    public void SaveLoadedAchievements(string storepath, string idpath)
+    {
+        FileStream fs = new FileStream(storepath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+        FileStream fs2 = new FileStream(idpath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+        BinaryFormatter bf = new BinaryFormatter();
+
+        bf.Serialize(fs, AchievementStore);
+        bf.Serialize(fs2, AchievemntIds);
+        
+        fs.Close();
+        fs2.Close();
+    }
+
+
+
+    //     ~ Startup
+    public bool[] LoadAllAchievements(string path)
+    {
+        if (File.Exists(path))
+        {
+            FileStream fs = new FileStream(path, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
+            BinaryFormatter bf = new BinaryFormatter();
+            fs.Position = 0;
+            return (bool[])bf.Deserialize(fs);
+        } 
+        return null;
+    }
+    public int[] LoadAchievementIDs(string path)
+    {
+        if (File.Exists(path))
+        {
+            FileStream fs = new FileStream(path, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
+            BinaryFormatter bf = new BinaryFormatter();
+            fs.Position = 0;
+            return (int[])bf.Deserialize(fs);
+        }
+        return null;
+    }
+
+
+    //     ~ Loader
+
+    internal class Loader
+    {
+        public void Load(string storepath, string idspath)
+        {
+            if (File.Exists("_ids.save"))
+            {
+                current.AchievementStore = current.LoadAllAchievements(storepath);
+                current.AchievemntIds = current.LoadAchievementIDs(idspath);
+            }
+
+        }
+    }
+ 
+}
